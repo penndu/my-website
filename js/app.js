82fcb9ff55c4b9708f80c22482a3b11b352998c2
penndu <penndu@outlook.com>
2022-01-27 22:51:53 +0800

    Site updated: 2022-01-27 22:51:48

--- js/app.js
+++ js/app.js
@@ -72,11 +72,9 @@ const VolantisApp = (() => {
     });
 
     // 站点信息 最后活动日期
-    if (document.getElementById('last-update-show')
-      && volantis.THEMECONFIG.sidebar.for_page.includes('webinfo')
-      || volantis.THEMECONFIG.sidebar.for_post.includes('webinfo')) {
+    if (volantis.THEMECONFIG.sidebar.for_page.includes('webinfo') || volantis.THEMECONFIG.sidebar.for_post.includes('webinfo')) {
       const lastupd = volantis.THEMECONFIG.sidebar.widget_library.webinfo.type.lastupd;
-      if (lastupd.enable && lastupd.friendlyShow) {
+      if (!!document.getElementById('last-update-show') && lastupd.enable && lastupd.friendlyShow) {
         document.getElementById('last-update-show').innerHTML = fn.utilTimeAgo(volantis.LASTUPDATE);
       }
     }
@@ -690,8 +688,8 @@ const VolantisFancyBox = (() => {
   const fn = {};
 
   fn.loadFancyBox = (done) => {
-    volantis.css('https://unpkg.com/@fancyapps/ui@4.0.12/dist/fancybox.css');
-    volantis.js('https://unpkg.com/@fancyapps/ui@4.0.12/dist/fancybox.umd.js').then(() => {
+    volantis.css(volantis.THEMECONFIG.plugins.fancybox.css);
+    volantis.js(volantis.THEMECONFIG.plugins.fancybox.js).then(() => {
       if (done) done();
     })
   }
